c7669ba6a48b3ea2195122ac33d52971c9e30af4
Visoth YOUK-SIM <115548934+visothyouksim@users.noreply.github.com>
2024-06-20 15:07:01 +0200

    écran divisé par deux avec fragment roug et bleu OK !!!!

--- app/src/main/java/com/example/projectfragment/MainActivity.kt
+++ app/src/main/java/com/example/projectfragment/MainActivity.kt
@@ -3,22 +3,38 @@ package com.example.projectfragment
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (savedInstanceState == null) {
-            val menuFragment = MenuFragment()
-            val contentFragment = ContentFragment()
+        val buttonToggleBlueFragment = findViewById<Button>(R.id.button_toggle_blue_fragment)
+        val buttonToggleRedFragment = findViewById<Button>(R.id.button_toggle_red_fragment)
 
-            supportFragmentManager.beginTransaction()
-                .add(R.id.menu_fragment_container, menuFragment)
-                .add(R.id.content_fragment_container, contentFragment)
-                .commit()
+        buttonToggleBlueFragment.setOnClickListener {
+            toggleFragment(BlueFragment(), "BLUE_FRAGMENT", R.id.blue_fragment_container)
         }
+
+        buttonToggleRedFragment.setOnClickListener {
+            toggleFragment(RedFragment(), "RED_FRAGMENT", R.id.red_fragment_container)
+        }
+    }
+
+    private fun toggleFragment(fragment: Fragment, tag: String, containerId: Int) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        val existingFragment = fragmentManager.findFragmentByTag(tag)
+        if (existingFragment == null) {
+            fragmentTransaction.add(containerId, fragment, tag)
+        } else {
+            fragmentTransaction.remove(existingFragment)
+        }
+
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+        fragmentTransaction.commit()
     }
 }
